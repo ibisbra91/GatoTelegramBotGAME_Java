8f8c09e1d27a704d0e5a0fba7c04d7afb94985b1
Ibis <ibisbra91@gmail.com>
2024-04-23 15:28:12 -0300

    Redical restructuring of the project. Deleted files generated by default that will not be used. Added JAVA marathon CodeGym project files. Images excluded.

---
+++ TelegramBot/src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -0,0 +1,26 @@
+package es.codegym.telegrambot;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class MyFirstTelegramBot extends MultiSessionTelegramBot {
+
+    public static final String NAME = "agregue el nombre del bot entre comillas a continuación";
+    public static final String TOKEN = "agregue el token del bot entre comillas a continuación";
+
+    public MyFirstTelegramBot() {
+        super(NAME, TOKEN);
+    }
+
+    @Override
+    public void onUpdateEventReceived(Update update) {
+        // TODO: escribiremos la funcionalidad principal del bot aquí
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new MyFirstTelegramBot());
+    }
+}
