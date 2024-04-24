886928d6ae74926af65f0ed3d12544a7106241eb
Ibis <ibisbra91@gmail.com>
2024-04-24 20:28:11 -0300

    Added to the .gitignore file the indication to ignore: the target directory (which contains the .class binaries) and the .idea directory. Changed the word pañales to bold in MyFirstTelegramBot.java class.

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -18,7 +18,7 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     @Override
     public void onUpdateEventReceived(Update update) {
         // TODO: escribiremos la funcionalidad principal del bot aquí
-        sendTextMessageAsync("Hola, soy catbot en _pañales_.");
+        sendTextMessageAsync("Hola, *soy catbot* en _pañales_.");
     }
 
     public static void main(String[] args) throws TelegramApiException {
