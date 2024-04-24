30b35ae1dc2cfafaa093e0d4495d5f1c422230dc
Ibis <ibisbra91@gmail.com>
2024-04-23 21:31:07 -0300

    Restructuring of the code to solve a bug related to the pom.xml file. Catbot generated on telegram web. Added values of the catbot NAME and TOKEN constants. Programmed initial behavior of the catbot, excuted and tested the connection.

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -7,8 +7,8 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
-    public static final String NAME = "agregue el nombre del bot entre comillas a continuación";
-    public static final String TOKEN = "agregue el token del bot entre comillas a continuación";
+    public static final String NAME = "C4tb0t_Gam3_1b1s_bot";
+    public static final String TOKEN = "6635129863:AAF3Ceac_1pIel61ktJ_bpwP1JsFHE2TZ_I";
 
     public MyFirstTelegramBot() {
         super(NAME, TOKEN);
@@ -17,6 +17,7 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
     @Override
     public void onUpdateEventReceived(Update update) {
         // TODO: escribiremos la funcionalidad principal del bot aquí
+        sendTextMessageAsync("Hola, soy catbot en pañales.");
     }
 
     public static void main(String[] args) throws TelegramApiException {
